fe8817e7ca07fdfe6ae9dc22d0f54045305cb9cf
iamhvsharma <itshvsharma@gmail.com>
2024-10-21 11:49:05 +0530

    Solana Wallet Adapter Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
-import './App.css'
+import "./App.css";
 
-import { TokenLaunchpad } from './components/TokenLaunchpad'
+// wallet adapter imports
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+// Import Token Launchpad Component
+import { TokenLaunchpad } from "./components/TokenLaunchpad";
 
 function App() {
   return (
-  <div>
-    <TokenLaunchpad></TokenLaunchpad>
-  </div>
-  )
+    <div>
+      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+        <WalletProvider wallets={[]} autoConnect>
+          <WalletModalProvider>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                padding: 20,
+              }}
+            >
+              <WalletMultiButton />
+              <WalletDisconnectButton />
+            </div>
+            <TokenLaunchpad></TokenLaunchpad>
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
